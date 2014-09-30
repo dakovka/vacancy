6807d30fc84a1c3f5419062a76de1d37bc70c1e6
Daria Prusova <guzhova@umisoft.ru>
2014-09-30 14:06:50 +0400

    doctrine integration

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,16 @@
  */
 
 return array(
-    // ...
+
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
+                'params' => array(
+                    'path'     => dirname(dirname(__DIR__)) . '/data/vacancy.db'
+                )
+            )
+        )
+    ),
+
 );
